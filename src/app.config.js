b38ccecc8cde4276794a5b3abe04b330d7331aac
ElectricMaxxx <maximilian.berghoff@gmx.de>
2015-07-09 08:49:16 +0200

    remove .js files from repo

--- src/app.config.js
+++
@@ -1,6 +0,0 @@
-/// <reference path='_all.ts' />
-angular.module('angularCmf.config', ['restangular'])
-    .config(function (RestangularProvider) {
-    RestangularProvider.setEncodeIds(false);
-    RestangularProvider.setBaseUrl('api');
-});
